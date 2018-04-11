c0acedb15d8f5bd284750961284bcde3b8900e9c
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-04-11 13:38:12 +0500

    Checkin on another comp

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -17,6 +17,7 @@ namespace Calculate
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //allah bomsh
         }
     }
 }
