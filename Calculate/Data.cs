08800d34f9b0249a4f17b2a477bed8b2b43192e5
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-04-13 09:07:09 +0500

    Pre-Alpha

--- Calculate/Data.cs
+++ Calculate/Data.cs
@@ -8,5 +8,26 @@ namespace Calculate
 {
     class Data
     {
+        private double a,b,c,d;
+        public double A 
+        {
+            get{return a;}
+            set{a=value;}
+        }
+        public double B 
+        {
+            get{return b;}
+            set{b=value;}
+        }
+        public double C 
+        {
+            get{return c;}
+            set{c=value;}
+        }
+        public double D 
+        {
+            get{return d;}
+            set{d=value;}
+        }
     }
 }
