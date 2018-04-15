bb1a1aef72a08ff343c130e7e2dc0dbfddd675e8
Savelii Ugrinhcuk <dudle1999@mail.ru>
2018-04-15 15:30:06 +0500

    Вычисление дополнительных определителей

--- Calculate/Actions.cs
+++ Calculate/Actions.cs
@@ -12,19 +12,56 @@ namespace Calculate
         /// <summary>
         /// Значения уравнений матрицы
         /// </summary> 
-        static protected double A1 = 0, B1 = 0, C1 = 0, D1 = 0;
-        static protected double A2 = 0, B2 = 0, C2 = 0, D2 = 0;
-        static protected double A3 = 0, B3 = 0, C3 = 0, D3 = 0;
+        protected double A1, B1, C1, D1;
+        protected double A2, B2, C2, D2;
+        protected double A3, B3, C3, D3;
 
 
-        static protected int lengthEnhanced = 4;
-        static protected int lengthBase = 3;
+        static protected int lengthEnhanced = 4;                                        //кол-во строк и столбцов расширенной матрицы
+        static protected int lengthBase = 3;                                            //кол-во строк и столбцов основной матрицы
 
-        double[,] masEnhanced = new double[lengthBase, lengthEnhanced];
-        double[,] masBase = new double[lengthBase, lengthBase];
+        protected double[,] masEnhanced = new double[lengthBase, lengthEnhanced];       //расширенная матрица
 
-        public static double[,] MasCreating(double[,] masEnhanced)
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Actions()
         {
+            A1 = 0; B1 = 0; C1 = 0; D1 = 0;
+            A2 = 0; B2 = 0; C2 = 0; D2 = 0;
+            A3 = 0; B3 = 0; C3 = 0; D3 = 0;
+        }
+
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="A1"></param>
+        /// <param name="A2"></param>
+        /// <param name="A3"></param>
+        /// <param name="B1"></param>
+        /// <param name="B2"></param>
+        /// <param name="B3"></param>
+        /// <param name="C1"></param>
+        /// <param name="C2"></param>
+        /// <param name="C3"></param>
+        /// <param name="D1"></param>
+        /// <param name="D2"></param>
+        /// <param name="D3"></param>
+        public Actions(double A1, double A2, double A3, double B1, double B2, double B3, double C1, double C2, double C3, double D1, double D2, double D3)
+        {
+            this.A1 = A1; this.B1 = B1; this.C1 = C1; this.D1 = D1;
+            this.A2 = A2; this.B2 = B2; this.C2 = C2; this.D2 = D2;
+            this.A3 = A3; this.B3 = B3; this.C3 = C3; this.D3 = D3;
+        }
+
+        /// <summary>
+        /// Создание матрицы
+        /// </summary>
+        /// <param name="masEnhanced"></param>
+        /// <returns></returns>
+        private double[,] MasCreating()
+        {
+            //присваиваем введенные значения
             masEnhanced[0, 0] = A1;
             masEnhanced[0, 1] = B1;
             masEnhanced[0, 2] = C1;
@@ -40,7 +77,6 @@ namespace Calculate
             masEnhanced[2, 2] = C3;
             masEnhanced[2, 3] = D3;
 
-
             return masEnhanced;
         }
 
@@ -49,11 +85,12 @@ namespace Calculate
         /// </summary>
         /// <param name="masEnhanced"></param>
         /// <returns></returns>
-        public static double OpredelitelEnhancedMatrix(double[,] masEnhanced)
+        private double DeterminantEnhancedMatrix()
         {
-            double Opredelitel = 0;
+            double Determinant;
             double per1, per2, per3, per4, per5, per6;
-
+            
+            //находим определитель через треугольники
             per1 = masEnhanced[0, 0] * masEnhanced[1, 1] * masEnhanced[2, 2];
             per2 = masEnhanced[0, 1] * masEnhanced[1, 2] * masEnhanced[2, 0];
             per3 = masEnhanced[0, 2] * masEnhanced[1, 0] * masEnhanced[2, 1];
@@ -61,8 +98,99 @@ namespace Calculate
             per5 = masEnhanced[0, 1] * masEnhanced[1, 0] * masEnhanced[2, 2];
             per6 = masEnhanced[0, 1] * masEnhanced[1, 2] * masEnhanced[2, 1];
 
-            Opredelitel = per1 - per2 - per3 - per4 - per5 - per6;
-            return Opredelitel;
+            Determinant = per1 - per2 - per3 - per4 - per5 - per6;
+            return Determinant;
         }
+
+        /// <summary>
+        /// Поиск 1 вспомогательного определителя
+        /// </summary>
+        /// <param name="masEnhanced"></param>
+        /// <returns></returns>
+        private double SupportDeterminant1()
+        {
+            double SupportDeterminant1;                                        //первый вспомогательный определитель
+            double[,] masSupport = new double[lengthBase,lengthBase];          //основная матрица
+            masSupport = masEnhanced;                                          //присваиваем значения основной матрице (без D)
+
+            masSupport[0, 0] = D1;
+            masSupport[1, 0] = D2;
+            masSupport[2, 0] = D3;
+
+            double per1, per2, per3, per4, per5, per6;
+
+            //находим определитель через треугольники
+            per1 = masSupport[0, 0] * masSupport[1, 1] * masSupport[2, 2];
+            per2 = masSupport[0, 1] * masSupport[1, 2] * masSupport[2, 0];
+            per3 = masSupport[0, 2] * masSupport[1, 0] * masSupport[2, 1];
+            per4 = masSupport[0, 2] * masSupport[1, 1] * masSupport[2, 0];
+            per5 = masSupport[0, 1] * masSupport[1, 0] * masSupport[2, 2];
+            per6 = masSupport[0, 1] * masSupport[1, 2] * masSupport[2, 1];
+
+            SupportDeterminant1 = per1 - per2 - per3 - per4 - per5 - per6;
+            return SupportDeterminant1;
+        }
+
+        /// <summary>
+        /// Поиск 2 вспомогательного определителя
+        /// </summary>
+        /// <param name="masEnhanced"></param>
+        /// <returns></returns>
+        private double SupportDeterminant2()
+        {
+            double SupportDeterminant2;                                         //второй вспомогательный определитель
+            double[,] masSupport = new double[lengthBase, lengthBase];          //основная матрица
+            masSupport = masEnhanced;                                           //присваиваем значения основной матрице (без D)
+
+            masSupport[0, 1] = D1;
+            masSupport[1, 1] = D2;
+            masSupport[2, 1] = D3;
+
+            double per1, per2, per3, per4, per5, per6;
+
+            //находим определитель через треугольники
+            per1 = masSupport[0, 0] * masSupport[1, 1] * masSupport[2, 2];
+            per2 = masSupport[0, 1] * masSupport[1, 2] * masSupport[2, 0];
+            per3 = masSupport[0, 2] * masSupport[1, 0] * masSupport[2, 1];
+            per4 = masSupport[0, 2] * masSupport[1, 1] * masSupport[2, 0];
+            per5 = masSupport[0, 1] * masSupport[1, 0] * masSupport[2, 2];
+            per6 = masSupport[0, 1] * masSupport[1, 2] * masSupport[2, 1];
+
+            SupportDeterminant2 = per1 - per2 - per3 - per4 - per5 - per6;
+            return SupportDeterminant2;
+        }
+
+        /// <summary>
+        /// Поиск 3 вспомогательного определителя
+        /// </summary>
+        /// <param name="masEnhanced"></param>
+        /// <returns></returns>
+        private double SupportDeterminant3()
+        {
+            double SupportDeterminant3;                                         //третий вспомогательный определитель
+            double[,] masSupport = new double[lengthBase, lengthBase];          //основная матрица
+            masSupport = masEnhanced;                                           //присваиваем значения основной матрице (без D)
+
+            masSupport[0, 2] = D1;
+            masSupport[1, 2] = D2;
+            masSupport[2, 2] = D3;
+
+            double per1, per2, per3, per4, per5, per6;
+
+            //находим определитель через треугольники
+            per1 = masSupport[0, 0] * masSupport[1, 1] * masSupport[2, 2];
+            per2 = masSupport[0, 1] * masSupport[1, 2] * masSupport[2, 0];
+            per3 = masSupport[0, 2] * masSupport[1, 0] * masSupport[2, 1];
+            per4 = masSupport[0, 2] * masSupport[1, 1] * masSupport[2, 0];
+            per5 = masSupport[0, 1] * masSupport[1, 0] * masSupport[2, 2];
+            per6 = masSupport[0, 1] * masSupport[1, 2] * masSupport[2, 1];
+
+            SupportDeterminant3 = per1 - per2 - per3 - per4 - per5 - per6;
+            return SupportDeterminant3;
+        }
+
+        
+
+
     }
 }
